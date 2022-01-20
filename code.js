e6faf0feadd60a975a06fa0de5e033e9b27539e0
EmerikwaNicholas <emerikwanicholas@gmail.com>
2022-01-20 12:30:49 +0300

    first commit

---
+++ code.js
@@ -0,0 +1,11 @@
+var height = 10;
+var radius = 5;
+const PI = 3.14;
+
+function Calculate(){
+    Area = PI * radius * radius;
+console.log("Area is: " +Area);
+    volume = Area * height;
+    console.log("Volume is: " +volume);
+}
+Calculate();
